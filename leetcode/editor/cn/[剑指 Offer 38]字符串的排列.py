554c869aec9d86b598cdee23e9f5063324c43e2f
萧 <1243925457@qq.com>
2021-06-22 22:19:15 +0800

    update

--- leetcode/editor/cn/[剑指 Offer 38]字符串的排列.py
+++ leetcode/editor/cn/[剑指 Offer 38]字符串的排列.py
@@ -27,19 +27,22 @@ from typing import List
 
 class Solution:
     def permutation(self, s: str) -> List[str]:
-        res_list = []
-        self.cal(res_list, s, "")
-        # print(len(res_list), res_list)
-        return res_list
+        c, res = list(s), []
 
-    def cal(self, res_list, chars, res):
-        # print(res_list, chars, res)
-        if len(chars) == 0:
-            if res not in res_list:
-                res_list.append(res)
-            return
-        for index in range(0, len(chars)):
-            self.cal(res_list, chars[:index] + chars[index+1:], res + chars[index])
+        def dfs(x):
+            if x == len(c) - 1:
+                res.append(''.join(c))  # 添加排列方案
+                return
+            dic = set()
+            for i in range(x, len(c)):
+                if c[i] in dic: continue  # 重复，因此剪枝
+                dic.add(c[i])
+                c[i], c[x] = c[x], c[i]  # 交换，将 c[i] 固定在第 x 位
+                dfs(x + 1)  # 开启固定第 x + 1 位字符
+                c[i], c[x] = c[x], c[i]  # 恢复交换
+
+        dfs(0)
+        return res
 # leetcode submit region end(Prohibit modification and deletion)
 
 
