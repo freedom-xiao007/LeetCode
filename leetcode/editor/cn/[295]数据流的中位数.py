cc5a063ca70e5bdaf17ff1b5b223e73dadaf80f4
萧 <1243925457@qq.com>
2021-08-27 08:51:01 +0800

    Create [295]数据流的中位数.py

---
+++ leetcode/editor/cn/[295]数据流的中位数.py
@@ -0,0 +1,65 @@
+# 中位数是有序列表中间的数。如果列表长度是偶数，中位数则是中间两个数的平均值。 
+# 
+#  例如， 
+# 
+#  [2,3,4] 的中位数是 3 
+# 
+#  [2,3] 的中位数是 (2 + 3) / 2 = 2.5 
+# 
+#  设计一个支持以下两种操作的数据结构： 
+# 
+#  
+#  void addNum(int num) - 从数据流中添加一个整数到数据结构中。 
+#  double findMedian() - 返回目前所有元素的中位数。 
+#  
+# 
+#  示例： 
+# 
+#  addNum(1)
+# addNum(2)
+# findMedian() -> 1.5
+# addNum(3) 
+# findMedian() -> 2 
+# 
+#  进阶: 
+# 
+#  
+#  如果数据流中所有整数都在 0 到 100 范围内，你将如何优化你的算法？ 
+#  如果数据流中 99% 的整数都在 0 到 100 范围内，你将如何优化你的算法？ 
+#  
+#  Related Topics 设计 双指针 数据流 排序 堆（优先队列） 
+#  👍 469 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+import heapq
+
+
+class MedianFinder:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        # 初始化大顶堆和小顶堆
+        self.max_heap = []
+        self.min_heap = []
+
+    def addNum(self, num: int) -> None:
+        if len(self.max_heap) == len(self.min_heap):# 先加到大顶堆，再把大堆顶元素加到小顶堆
+            heapq.heappush(self.min_heap, -heapq.heappushpop(self.max_heap, -num))
+        else:  # 先加到小顶堆，再把小堆顶元素加到大顶堆
+            heapq.heappush(self.max_heap, -heapq.heappushpop(self.min_heap, num))
+
+    def findMedian(self) -> float:
+        if len(self.min_heap) == len(self.max_heap):
+            return (-self.max_heap[0] + self.min_heap[0]) / 2
+        else:
+            return self.min_heap[0]
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
+# leetcode submit region end(Prohibit modification and deletion)
