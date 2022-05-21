9b7e96479cc35cb8d9bb96c8e4d502b792292b9a
萧 <1243925457@qq.com>
2022-05-21 19:55:13 +0800

    Update [436]寻找右区间.py

--- leetcode/editor/cn/[436]寻找右区间.py
+++ leetcode/editor/cn/[436]寻找右区间.py
@@ -48,4 +48,5 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
+        
 # leetcode submit region end(Prohibit modification and deletion)
